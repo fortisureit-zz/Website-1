c7335f4a35cdea5819b62234203304eefe016014
ajbarbati <ajbarbati@gmail.com>
2020-02-25 12:44:20 -0500

    server

--- src/server.js
+++ src/server.js
@@ -14,10 +14,10 @@ app.listen(process.env.PORT || port, () =>
 
 app.use(express.static('public'))
 
-// app.use(function(req, res, next){
-//   res.header("Content-Security-Policy", "default-src 'self';script-src 'https://www.google.com/recaptcha/api.js'");
-//   next();
-// })
+app.use(function(req, res, next){
+  res.header("Content-Security-Policy", "default-src 'self';script-src 'unsafe-inline'");
+  next();
+})
 
 app.use(
   csp({
@@ -36,7 +36,7 @@ app.use(
       ],
       'style-src': [csp.SELF, csp.INLINE, 'https://fonts.googleapis.com/'],
       'font-src': [csp.SELF, 'https://fonts.gstatic.com'],
-      'script-src': [csp.SELF, csp.INLINE, csp.NONCE, 'EDNnf03nceIOfn39fn3e9h3sdfa' 'https://www.google.com/recaptcha/', "https://www.gstatic.com/recaptcha/", 'https://www.google.com/recaptcha/api.js'],
+      'script-src': [csp.SELF, csp.INLINE, 'https://www.google.com/recaptcha/', "https://www.gstatic.com/recaptcha/", 'https://www.google.com/recaptcha/api.js'],
       'worker-src': [csp.NONE],
       'frame-src': ['https://www.google.com/recaptcha/'],
       'media-src': [
